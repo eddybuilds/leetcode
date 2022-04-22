9cb62f8ef2d4393230098d7c3431ebfc40ee266b
eddybuilds <hey@eddybuilds.io>
2022-04-21 20:22:43 -0700

    Added some binary trees

---
+++ src/trees/README.md
@@ -0,0 +1,10 @@
+# Trees
+
+## Binary Trees 
+
+A binary tree is made of nodes, where each node contains a "left" pointer, a "right" pointer, and a data element. Formally, a binary tree is either empty (represented by a null pointer), or is made of a single node, where the left and right pointers (recursive definition ahead) each point to a binary tree. 
+
+A binary search tree, or ordered binary tree, is a type of binary tree where the nodes are arranged in order: for each node all elements in its left subtree are less-or-equal to the node and all values in its right subtree are greater than the node. 
+
+A binary search tree is *different* to a binary tree. 
+
---
+++ src/trees/binarySearchTree.js
@@ -0,0 +1,69 @@
+class Node {
+  constructor(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+function newNode(value) {
+  return new Node(value, null, null);
+}
+
+function insert(node, data) {
+  if (node == null) {
+    return newNode(data);
+  }
+
+  if (data <= node.data) {
+    node.left = insert(node.left, data);
+  } 
+  else {
+    node.right = insert(node.right, data);
+  }
+
+  return node;
+}
+
+function lookup(node, target) {
+  if (node == null) {
+    return false;
+  }
+
+  if (target == node.data) {
+    return true;
+  }
+
+  if (target < node.data) {
+    return lookup(node.left, target);
+  }
+  else {
+    return lookup(node.right, target);
+  }
+}
+
+let root = new Node(9, new Node(4, null, null), new Node(11, null, null));
+console.log(root);
+/*
+Node {
+  data: 9,
+  left: Node { data: 4, left: null, right: null },
+  right: Node { data: 11, left: null, right: null }
+}
+*/
+console.log(insert(root, 3));
+/*
+Node {
+  data: 9,
+  left: Node {
+    data: 4,
+    left: Node { data: 3, left: null, right: null },
+    right: null
+  },
+  right: Node { data: 11, left: null, right: null }
+}
+*/
+console.log(lookup(root, 3));
+/*
+true
+*/
